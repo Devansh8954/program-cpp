d26b8e7d57449642c30482f346f79a87c5fef7e7
Devansh <devansh8954@gmail.com>
2022-08-01 20:58:51 +0530

    added pattern8

---
+++ pattern programs/pattern8.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+     int N,i=1;
+    cin >> N;
+    while(i<=N)
+    {
+        int j=1;
+        while(j<=i)
+        {
+            char ch=  'A'+i-1;
+            cout << ch;
+            j++;
+        }
+        cout<< endl;
+        i++;
+    }
+    return 0;
+}
