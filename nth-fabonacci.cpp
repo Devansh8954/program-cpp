a016ea1ce274e6dd9ed6be62c8795bf17c4e6d4b
Devansh <devansh8954@gmail.com>
2022-08-19 22:32:13 +0530

    added nth fabonacci program

--- nth-fabonacci.cpp
+++ nth-fabonacci.cpp
@@ -5,30 +5,23 @@ int main() {
     //Write your code here
     int n;
     cin>>n;
-  int m=1000000007;
 
-     int first=0,second=1;
+     int first=0,second=0;
 
        int fibo=1;
 
+       if(n==0) 
+           cout<<0;
 
-       if(n==1) 
-       {
-        cout<< "1";
-       }
-       else if(n==2)
-       {
-        cout<< "1";
-       }
+       else if(n==1) 
+           cout << 1;
+
        else {
         for(int i=2;i<=n;i++){
 
-           fibo=(first+second)%m;
-
            first=second;
-
            second=fibo;
-
+           fibo=first+second; 
        }
 
        cout<< fibo;
