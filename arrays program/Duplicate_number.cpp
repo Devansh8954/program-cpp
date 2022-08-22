68b36ec5e60c57e6bae3180778f988df62387891
Devansh <devansh8954@gmail.com>
2022-08-22 19:55:30 +0530

    duplicate number program

---
+++ arrays program/Duplicate_number.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int duplicateNumber(int *arr, int size)
+{
+    //Write your code here
+    for(int i=0;i<size; i++)
+    {
+        int jg=-1;
+        for(int j=0; j<size; j++)
+        {
+            if (arr[i]==arr[j])
+                    {
+                        jg++;
+                    }
+        }
+        if (jg>0)
+            {
+                return arr[i];
+            }
+    }
+     return arr[0];
+    
+}
+
+int main()
+{
+
+	int t;
+	cin >> t;
+	
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+
+		cout << duplicateNumber(input, size) << endl;
+	}
+
+	return 0;
+}
