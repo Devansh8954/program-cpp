7c827805f6c32be9e3efe78cfe2d1e1bf535bf6a
Devansh <devansh8954@gmail.com>
2022-09-20 20:30:40 +0530

    added 1 more func

--- DSA/Recursions/Count_Zeros.cpp
+++ DSA/Recursions/Count_Zeros.cpp
@@ -61,6 +61,23 @@ int countZeros(int n)
     return smalloutput;
 }
 
+int countZeros(int n) {
+    // Write your code here
+static int count=0;
+    if(n>0)
+    {
+
+        if(n%10==0)
+        count++;
+
+       countZeros(n/10);
+    }else if(n==0){
+        return 1;
+    }
+    return count;
+
+}
+
 int main()
 {
     int n;
