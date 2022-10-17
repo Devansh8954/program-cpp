c163ee863c7f026f22ae95026d743578dd13ae20
Devansh <devansh8954@gmail.com>
2022-10-17 19:36:18 +0530

    added example of Dynamic allocating linkedlist

---
+++ DSA/Linkedlist/Dynamic_allocating.cpp
@@ -0,0 +1,41 @@
+#include <cstddef>
+#include<iostream>
+
+class Node{
+    public:
+        int data;
+        Node *next;
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+using namespace std;
+void print(Node *head){
+    Node *temp = head;
+
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+void increment(Node *head){
+    Node *temp = head;
+    while(temp != NULL){
+        temp -> data++;
+        temp = temp -> next;
+    }
+}
+
+int main(){
+
+    Node *node1 = new Node(10);
+    Node *node2 = new Node(20);
+    node1 -> next = node2;
+    increment(node1);
+    print(node1);
+}
