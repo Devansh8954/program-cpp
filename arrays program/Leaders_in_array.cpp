7e7dbd6477977591f2da614f3f35e5dc858a74a6
Devansh <devansh8954@gmail.com>
2023-02-09 21:35:38 +0530

    updated

--- arrays program/Leaders_in_array.cpp
+++ arrays program/Leaders_in_array.cpp
@@ -1,11 +1,12 @@
 /*
-Given an integer array A of size n. Find and print all the leaders present in the input array. An array element A[i] is called Leader, if all the elements following it (i.e. present at its right) are less than or equal to A[i].
+Given an integer array A of size n. Find and print all the leaders present in the input array. An array element A[i] is called Leader, if all the 
+elements following it (i.e. present at its right) are less than or equal to A[i].
 Print all the leader elements separated by space and in the same order they are present in the input array.
 Input Format :
 Line 1 : Integer n, size of array
 Line 2 : Array A elements (separated by space)
 Output Format :
- leaders of array (separated by space)
+Leaders of array (separated by space)
 Constraints :
 1 <= n <= 10^6
 Sample Input 1 :
