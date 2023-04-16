62d734bdd36c3f7e7e873ea7a33a0ff25d36969e
DEVANSH TYAGI <96236567+Devansh8954@users.noreply.github.com>
2023-04-16 21:53:21 +0530

    added coin tower

---
+++ DSA/DP-2/Coin_Tower.cpp
@@ -0,0 +1,61 @@
+/*Coin Tower
+Whis and Beerus are playing a new game today. They form a tower of N coins and make a move in alternate turns. Beerus plays first. 
+In one step, the player can remove either 1, X, or Y coins from the tower. The person to make the last move wins the game. Can you
+ find out who wins the game?
+ Input format :
+The first and the only line of input contains three integer values separated by a single space. They denote the value of N, X and Y,
+ respectively.
+Output format :
+Prints the name of the winner, either 'Whis' or 'Beerus' (Without the quotes).
+Constraints :
+1 <= N <= 10 ^ 6
+2 <= X <= Y<= 50
+
+Time Limit: 1 sec
+Sample Input 1 :
+4 2 3
+Sample Output 1 :
+Whis
+Sample Input 2 :
+10 2 4
+Sample Output 2 :
+Beerus
+Explanation to Sample Input 1:
+Initially, there are 4 coins,  In the first move, Beerus can remove either 1, 2, or 3 coins in all three cases, Whis can win by removing
+all the remaining coins. */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string findWinner(int n, int x, int y) {
+  // Write your code here .
+  int *arr = new int[n + 1];
+  arr[0] = 0;
+  arr[1] = 1;
+  for (int i = 2; i <= n; i++) {
+    int a = 0, b = 0, c = 0;
+    a = arr[i - 1] ^ 1;
+    if (i - x >= 0)
+      b = arr[i - x] ^ 1;
+    if (i - y >= 0)
+      c = arr[i - y] ^ 1;
+    arr[i] = max(a, max(b, c));
+  }
+
+  if (arr[n] == 1)
+    return "Beerus";
+  else
+    return "Whis";
+}
+
+
+int main()
+{
+	int n, x, y;
+	cin >> n >> x >> y;
+	cout << findWinner(n, x, y);
+}
